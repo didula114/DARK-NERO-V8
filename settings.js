fec484481be97b88175e1ad2be4cf431621a4089
didula114 <127202806+didula114@users.noreply.github.com>
2023-03-17 13:19:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,30 +15,30 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.ANTI_BADWORD = 'true' // true of false
-global.AUTO_REACT = 'false' // true of false
+global.AUTO_REACT = 'true' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
-global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
+global.alivemsg = 'ᴅɪɴᴜᴡᴀ ᴏꜰꜰɪᴄɪᴀʟ࿐'
+global.alivepic = 'https://i.ibb.co/YQCgkjQ/20230216-135729.jpg '
+global.owner = ['0717845122'] //ur owner number
+global.ownername = "Dinuka" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.ownernomer = "94717845122" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
+global.premium = ['94717845122'] //ur premium number
+global.botname = 'ᴅɪɴᴜᴡᴀ-ᴏꜰꜰɪᴄɪᴀʟ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
+global.linkz = "https://chat.whatsapp.com/I655HNBaWIqDdi0YWlGAfW" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://www.darknero.ga/' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.packname = "Dinux official By" //ur sticker watermark packname
+global.author = "Dinux officiL Creating By Dinuka" //ur sticker watermark author
+global.wm = "Dinux official Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
